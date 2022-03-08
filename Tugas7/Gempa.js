974877a3449851d86186741447e19d5e80090636
tripng <triwahyu.png@gmail.com>
2022-03-08 16:55:13 +0700

    Add Tugas 7

---
+++ Tugas7/Gempa.js
@@ -0,0 +1,27 @@
+class Gempa{
+    constructor(lokasi,skala){
+        this.lokasi = lokasi;
+        this.skala = skala;
+    }
+    dampak(){
+        for (const iterator in this.lokasi) {
+            let randomSkala = Math.floor(Math.random()*this.skala) + 1,
+                dampak = (randomSkala>=0 && randomSkala<=2) ? "Tidak Berasa"
+                        : (randomSkala >= 2 && randomSkala <= 4) ? "Bangunan Retak"
+                        : (randomSkala >= 4 && randomSkala <= 6) ? "Bangunan Roboh"
+                        : (randomSkala > 6) ? "Bangunan Roboh Dan Berpotensi Tsunami"
+                        : "Nilai Skala Tidak Valid";
+            document.querySelector('tbody').innerHTML += `
+                <tr>
+                    <td> ${Number(iterator)+1} </td>
+                    <td> ${this.lokasi[iterator]} </td>
+                    <td> ${randomSkala} </td>
+                    <td> ${dampak} </td>
+                </tr>
+            `
+        }
+    }
+}
+let lokasi = ['Indonesia','Japan','Korea','Argentina','German','Turki','Swedia','Irlandia','India','Slokavia'],
+    gempaDunia = new Gempa(lokasi,10);
+    gempaDunia.dampak();
