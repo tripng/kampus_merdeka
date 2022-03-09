912de6c269b0785d831a38a6652a5f05e956ea63
Tri Pananggung <99007214+tripng@users.noreply.github.com>
2022-03-09 08:54:46 +0700

    Delete Tugas6 directory

--- Tugas6/JS_tugas6.js
+++
@@ -1,5 +0,0 @@
-let a1 = document.kalkulator.a1,
-    a2 = document.kalkulator.a2,
-    hasil = document.kalkulator.hasil;
-const hitung = cek => ((isNaN(a1.value) || a1.value=="") || (isNaN(a2.value) || a2.value=="")) ? 
-    alert("Input Data Tidak Valid") : hasil.value = eval(`${a1.value} ${cek} ${a2.value}`);
