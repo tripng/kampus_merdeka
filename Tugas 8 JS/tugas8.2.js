a91eb56e24e02056398338daa1393151b61f4b55
tripng <triwahyu.png@gmail.com>
2022-03-09 18:50:20 +0700

    fix bug

--- Tugas 8 JS/tugas8.2.js
+++ Tugas 8 JS/tugas8.2.js
@@ -7,13 +7,11 @@ $(document).ready(function(){
             });
             $(`.card.${i} p`).css({'display':'none'});
             $(`audio.${i}`).trigger("play");
-            for(let j=1; j<=4; j++){
-                if(j!=i){
-                    $(`audio.${j}`).trigger("pause");
-                    $(`.card.${j}`).css({'background' : `rgba(0, 0, 0, .4)`});
-                    $(`.card.${j} p`).css({'display':'block'});
-                } 
-            }
+            for(let j=1; j<=4; j++) if(j!=i){
+                $(`audio.${j}`).trigger("pause");
+                $(`.card.${j}`).css({'background' : `rgba(0, 0, 0, .4)`});
+                $(`.card.${j} p`).css({'display':'block'});
+            } 
         });
     }
 });
