7648dc6c23a07a4b723f92ab6c18061cf76724c2
tripng <triwahyu.png@gmail.com>
2022-03-10 08:37:30 +0700

    fix

--- Tugas 8 JS/tugas8.1.js
+++ Tugas 8 JS/tugas8.1.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     for(let i=1; i<=3; i++){
         $(`button:nth-child(${i})`).click(function(){ 
-            $(`.content.${i}`).slideToggle("click");
+            $(`.content.${i}`).slideToggle("slow");
             for(let j=1; j<=3; j++) if (j!=i) $(`.content.${j}`).css("display","none");
         });
     }
