5bacf9b895fceac765d268f5992262e5382300ed
tripng <triwahyu.png@gmail.com>
2022-03-10 17:06:14 +0700

    delete tugas 8

--- Tugas 8 JS/tugas8.1.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    for(let i=1; i<=3; i++){
-        $(`button:nth-child(${i})`).click(function(){ 
-            $(`.content.${i}`).slideToggle("slow");
-            for(let j=1; j<=3; j++) if (j!=i) $(`.content.${j}`).css("display","none");
-        });
-    }
-});
